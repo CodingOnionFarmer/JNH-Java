dfa4cfd1b724321b8dac0c09dfdef2ddcb9b9039
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2023-03-21 16:59:18 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 112 ms, Memory: 12952 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		l --;
+		l /=5;
+		System.out.println(l+1);
+	}
+}
