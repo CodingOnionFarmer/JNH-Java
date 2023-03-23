a7788205b34891d2660de6d4e1bf441b967ffedc
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2023-03-23 14:15:55 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11184 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print((int)sc.next().charAt(0));
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
