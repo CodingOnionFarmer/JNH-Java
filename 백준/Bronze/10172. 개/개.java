e4342b1b8991745e01a9c86ffa5ca1b35987405d
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2023-03-23 14:18:18 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11144 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print((int)sc.next().charAt(0));
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
